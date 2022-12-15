58d9ce975003e112e773b3f757e51de8ac68e03e
Ankit Toppo <67407155+anikoptimus@users.noreply.github.com>
2022-12-15 12:17:36 +0530

    Add files via upload

---
+++ Assets/scripts/SpawnerScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerScript : MonoBehaviour
+{
+    private float minX = -2.55f;
+    private float maxX = 2.55f;
+    public GameObject bombPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpwanBomb());
+    }// end satrt
+
+    IEnumerator SpwanBomb()
+    {
+        yield return new WaitForSeconds(Random.Range(0f, 1f));
+
+        Instantiate(bombPrefab, new Vector2(Random.Range(minX, maxX), transform.position.y),
+            Quaternion.identity);
+
+        StartCoroutine(SpwanBomb());
+    }
+
+}//class
+
